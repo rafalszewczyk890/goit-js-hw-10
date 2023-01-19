b2532bb632b5a93ce166546df9b5422258f8fa95
rafalszewczyk890 <rafal.szewczyk890@gmail.com>
2023-01-19 19:23:51 +0100

    Changed filtering method

--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ const DEBOUNCE_DELAY = 300;
 
 function fetchCountries(name) {
   fetch(
-    `https://restcountries.com/v3.1/name/${name}?fields=name.official,capital,population,flags.svg,languages`
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
   )
     .then(result => result.json())
-    .then(json => console.log(json));
+    .then(json =>
+      console.log(
+        json.capital,
+        json.flags.svg,
+        json.languages,
+        json.name.official
+      )
+    );
 }
 
 fetchCountries('japan');
