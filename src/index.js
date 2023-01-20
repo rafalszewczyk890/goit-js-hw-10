9a5aeb420a233b041d7195d75fea5cfde4cf7f21
rafalszewczyk890 <rafal.szewczyk890@gmail.com>
2023-01-20 14:07:03 +0100

    Return to start

--- src/index.js
+++ src/index.js
@@ -1,20 +1,3 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
-
-function fetchCountries(name) {
-  fetch(
-    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
-  )
-    .then(result => result.json())
-    .then(json =>
-      console.log(
-        json.capital,
-        json.flags.svg,
-        json.languages,
-        json.name.official
-      )
-    );
-}
-
-fetchCountries('japan');
